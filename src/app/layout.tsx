38b40a443fdae5984e827f8c9e54bb118878acaf
Karine Pereira <karinesp89@gmail.com>
2024-10-22 22:33:33 +0100

    storybook config and first stories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
 import StyledComponentsRegistry from '@/lib/registry'
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900'
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900'
-})
+import type { Metadata } from 'next'
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,10 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      <body>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
