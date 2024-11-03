05bfc33f57549e4917de46a8fb874f4c927ec06d
Karine Pereira <karinesp89@gmail.com>
2024-11-03 23:51:01 +0000

    Add plop to generate files

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -4,9 +4,24 @@ import { Main } from '.'
 export default {
   title: 'Main',
   component: Main,
+  argTypes: {
+    title: {
+      control: { type: 'text' }
+    },
+    description: {
+      control: { type: 'text' }
+    }
+  },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'Basic',
+    description: 'Description basic'
+  }
+}
